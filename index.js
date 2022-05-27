08a24e677cfc7da3a21ea8f795e465eb8447e4b4
Gia Bao <giabao.nguyenhoang1001@gmail.com>
2022-05-27 10:06:24 +0700

    add yads.ejs

--- index.js
+++ index.js
@@ -15,23 +15,35 @@ app.set('views', path.join(__dirname,'/views'));
 // =============== GET METHOD ======================
 // GET request to the specified path
 // with the specified callback functions.
+// ============ HOME PAGE ====================
 app.get('/', (req,res) => {
     // Render a view & send the rendered
     // HTML string to the client
     res.render('home');
 })
+
+// ============ RANDOM NUMBER PAGE ====================
 app.get('/random', (req,res) => {
+    // Create a random number
     const randNum = Math.floor(Math.random()*10)+1990;
     // randNum = variable that store value
     // tempNum = argument that use in the template
     res.render('random', { tempNum: randNum }); // [locals] = an object
 })
+
+// ============ RANDOM NUMBER PAGE ====================
 app.get('/r/:subreddit', (req, res) => {
     // req.params = an object
     const { subreddit } = req.params;
     // console.log(req.params);
     // console.log(subreddit);
     res.render('subreddit', {name: subreddit})
+})
+
+// ============ Loops in EJS PAGE ====================
+app.get('/yads', (req,res) => {
+    const yadMember = ['Bao', 'Thi', 'Dai', 'Truong', 'Phuoc', 'Thao', 'Hiep', 'Trang', 'Hieu', 'Hanh', 'Hoang'];
+    res.render('yads',{ yadMember: yadMember});
 })
 
 // Listen for connections on the specified
