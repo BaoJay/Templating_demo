d7f914339690457902f4271194bafcc30355991d
Gia Bao <giabao.nguyenhoang1001@gmail.com>
2022-05-27 12:13:13 +0700

    add notfound.ejs and create public folder

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ const path = require('path');
 // Retrieve data from ./data.json file
 const redditData = require('./data.json');
 
+app.use(express.static('public'));
+
 // Tell my app to use EJS template
 app.set('view engine', 'ejs');
 
-// Setting the view directory to run outside
+// Setting the view directory to run outside app directory
 app.set('views', path.join(__dirname,'/views'));
 
 // =============== GET METHOD ======================
@@ -44,10 +46,11 @@ app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
     // Query the data from redditData by passing a parameter
     const data = redditData[subreddit];
-    // const posts = data.posts;
-    // console.log(data);
-    // console.log(subreddit);
-    res.render('subreddit', {...data})
+    if (data) {
+        res.render('subreddit', {...data})
+    } else {
+        res.render('notfound', {subreddit})
+    }
 })
 
 // ============ Loops in EJS PAGE ====================
@@ -55,6 +58,8 @@ app.get('/yads', (req,res) => {
     const yadMember = ['Bao', 'Thi', 'Dai', 'Truong', 'Phuoc', 'Thao', 'Hiep', 'Trang', 'Hieu', 'Hanh', 'Hoang'];
     res.render('yads',{ yadMember: yadMember});
 })
+
+
 
 // Listen for connections on the specified
 // host and port 3000
